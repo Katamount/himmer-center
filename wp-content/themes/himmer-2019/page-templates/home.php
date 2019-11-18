003cd2851921dfb731469c7adb69b401dfea746b
Andrew Himmer <achimmer@gmail.com>
2019-11-17 21:53:58 -0600

    Updated and upgraded himmer-center-2019 theme dependecies with more Understrap upgrades

--- wp-content/themes/himmer-2019/page-templates/home.php
+++ wp-content/themes/himmer-2019/page-templates/home.php
@@ -21,13 +21,13 @@ get_header();
 			<div class="row">
 				<div class="col col-12 col-md-6">
 					<div class="perscription">
-						<img class="rx-icon" src="https://s3-us-west-2.amazonaws.com/katamount-media/misc/icon-rx-logo.png" >
+						<img class="rx-icon" src="https://himmer-center.s3.us-east-2.amazonaws.com/icons/icon-rx-logo.png" >
 						<h2 class="mission-stmt"><b>Organizations</b><br> need a healthy culture</h2>
 					</div>
 				</div>
 				<div class="col col-12 col-md-6">
 					<div class="perscription">
-						<img class="rx-icon" src="https://s3-us-west-2.amazonaws.com/katamount-media/misc/icon-rx-logo.png" >
+						<img class="rx-icon" src="https://himmer-center.s3.us-east-2.amazonaws.com/icons/icon-rx-logo.png" >
 						<h2 class="mission-stmt"><b>Individuals</b><br> need a safe environment</h2>
 					</div>
 				</div>
@@ -37,49 +37,21 @@ get_header();
 	</section>
 	<section class="section services ss-style-doublediagonal">
 		<div class="container">
+			<header class="justify-center">
+				<h1>Organizational options</h1>
+			</header>
 			<div class="row">
-				<div class="col col-12 col-lg-6 problems">
-					<h2>Challenges I address</h2>
+				<div class="col col-4 col-lg-4">
+					<img src ="https://himmer-center.s3.us-east-2.amazonaws.com/icons/icon-lecture.png">
+					<h3>Training Workshop</h3>
+					<p>Training workshops are usually department-team and/or purpose focused.</p>
 					<ul>
-						<li>
-							<h3>Employee engagement &amp; rentention</h3>
-							<p>The single most important concern of any organization is the well-being of their personnel. Employees are people, not assets. They drive the organization (sales, customer service, production, R & D, engineering, software development, etc.). Employees who trust their company and especially their immediate supervisor are engaged, innovative, and tend to stay with their companies.</p>
-						</li>
-						<li>
-							<h3>Leaders vs. managers</h3>
-							<p>It seems too many “managers” perceive themselves as leaders yet turn their direct reports into objects because they don’t have sufficient social skills (<abbr title="Emotional Intelligence">EI</abbr>) to deal with the challenges, differences, aptitudes, and addictions employees bring to work every day.</p>
-						</li>
-						<li>
-							<h3>Team building</h3>
-							<p>Team building stems from a culture of trust and respect. Innovation and emotional safety build unity and unity drives performance. A culture of life-long-learning (growth mindset) trumps motivational seminars and critical, threatening supervisors trying to hit short-term quotas.</p>
-						</li>
-						<li>
-							<h3>Workplace bullying</h3>
-							<p>A bully never operates alone. He or she always has an accomplice. The coworkers who keep their silence do so out of fear that they are next. Workplace bullying happens because the leaders and the culture of an organization invite it to happen. They permit it, they encourage it, and they reward it.</p>
-						</li>
-					</ul>
-				</div><!-- .col -->
-				<div class="col col-12 col-lg-6 methods">
-					<h2>Services I provide</h2>
+						<li>Engineering department training on Emotional Intelligence</li>
+						<li>Sales department on increasing their closing ratios</li>
+						<li>Leaders on improving communication and innovation</li>
 					<ul>
-						<li>
-							<img class="service-icon" src="https://s3-us-west-2.amazonaws.com/katamount-media/misc/icon-lecture.png" >
-							<h3>Training workshops</h3>
-						</li>
-						<li>
-							<img class="service-icon" src="https://s3-us-west-2.amazonaws.com/katamount-media/misc/icon-keynote.png" >
-							<h3>Keynote speaker</h3>
-						</li>
-						<li>
-							<img class="service-icon" src="https://s3-us-west-2.amazonaws.com/katamount-media/misc/icon-consultation.png" >
-							<h3>Personal consultation</h3>
-						</li>
-						<li>
-							<img class="service-icon" src="https://s3-us-west-2.amazonaws.com/katamount-media/misc/icon-couples-coaching.png" >
-							<h3>Couples coaching</h3>
-						</li>
-					</ul>
 				</div><!-- .col -->
+
 			</div><!-- .row -->
 			<h3 class="services-cta">You don’t encounter these problems?<br> <span>Prove it.</span></h3>
 			<p class="text-center"><a class="btn btn-lg btn-dark" href="contact">Email me</a></p>
