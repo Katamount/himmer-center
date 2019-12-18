781d88e1e0e5737b619ebd4ec89c80140eb8b23a
Andrew Himmer <achimmer@gmail.com>
2019-12-18 13:33:18 -0600

    uploading about page template

---
+++ wp-content/themes/himmer-2019/page-templates/about.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: About
+ *
+ * Custom template for About page
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+?>
+<!-- == Home == -->
+<main class="site-main about-page" id="main" role="main">
+  <section class="section">
+    <div class="container">
+      <h1>About Dr. Himmer</h1>
+      <p>Dr. Himmer is a Professional Keynote speaker and workshop facilitator. He is an author and organizational consultant. He coaches executives, entrepreneurs, couples, parents, and individuals who want to transform and grow. He received his bachelors degree from Brigham Young University, his MBA from City University, his masters in psychology and his PhD in Industrial/Organizational psychology from Walden University.</p>
+    </div>
+  </section>
+
+</main><!-- #main -->
+
+
+<?php get_footer(); ?>
