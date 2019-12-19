51f0a41bea5d2c42c4d73b9ca7109d93869d9600
Andrew Himmer <achimmer@gmail.com>
2019-12-19 08:13:26 -0600

    update to about page

--- wp-content/themes/himmer-2019/page-templates/about.php
+++ wp-content/themes/himmer-2019/page-templates/about.php
@@ -20,7 +20,8 @@ get_header();
       <div class="row">
         <div class="col-12 col-md-6">
           <h1>About Dr. Himmer</h1>
-          <p>Dr. Himmer is a Professional Keynote speaker and workshop facilitator. He is an author and organizational consultant. He coaches executives, entrepreneurs, couples, parents, and individuals who want to transform and grow. He received his bachelors degree from Brigham Young University, his MBA from City University, his masters in psychology and his PhD in Industrial/Organizational psychology from Walden University.</p>
+          <p>Dr. Himmer is a Professional Keynote speaker and workshop facilitator. He is an author and organizational consultant. He coaches executives, entrepreneurs, couples, parents, and individuals who want to transform and grow.</p> 
+          <p>He received his bachelors degree from Brigham Young University, his MBA from City University, his masters in psychology and his PhD in Industrial/Organizational psychology from Walden University.</p>
         </div>
         <div class="col-12 col-md-6">
           <img src="">
@@ -28,9 +29,11 @@ get_header();
       </div>
     </div>
   </section>
-  <section>
-    <div class="embed-responsive embed-responsive-16by9">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/KHxVxO1R880" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <section class="section">
+    <div class="container">
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/KHxVxO1R880" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
     </div>
   </section>
 
